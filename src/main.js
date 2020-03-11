50741114c4dd8324e5a9e25fe844a061a2de3f81
yutiansut <yutiansut@qq.com>
2020-03-12 07:06:00 +0800

    #

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import i18n from './i18n'
 import App from './App'
 // 核心插件
 import d2Admin from '@/plugin/d2admin'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import D2Crud from '@d2-projects/d2-crud'
+
 // store
 import store from '@/store/index'
 
@@ -15,7 +19,8 @@ import { frameInRoutes } from '@/router/routes'
 
 // 核心插件
 Vue.use(d2Admin)
-
+Vue.use(ElementUI)
+Vue.use(D2Crud)
 new Vue({
   router,
   store,
